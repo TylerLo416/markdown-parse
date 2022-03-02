c4c1a9d704abafe4615fc0e37d327a222f35c75e
TylerLo416 <93793209+TylerLo416@users.noreply.github.com>
2022-03-01 22:42:10 -0800

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -7,12 +7,59 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 public class MarkdownParseTest {
-    /*
     @Test
-    public void addition() {
-        assertEquals(2, 1 + 1);
+    public void testSnippet1() throws IOException {
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("`google.com");
+        expected.add("google.com");
+        expected.add("ucsd.edu");
+        List<String> list = List.of("snippet1.md");
+        for (String string : list) {
+            Path fileName = Path.of(string);
+	        String contents = Files.readString(fileName);
+            ArrayList<String> links = MarkdownParse.getLinks(contents);
+            assertEquals(expected, links);
+        }
     }
-    */
+    @Test
+    public void testSnippet2() throws IOException {
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("a.com");
+        expected.add("a.com(())");
+        expected.add("example.edu");
+        List<String> list = List.of("snippet2.md");
+
+        for (String string : list) {
+            Path fileName = Path.of(string);
+	        String contents = Files.readString(fileName);
+            ArrayList<String> links = MarkdownParse.getLinks(contents);
+            assertEquals(expected, links);
+        }
+    }
+    @Test
+    public void testSnippet3() throws IOException {
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("https://www.twitter.com");
+        expected.add("https://ucsd-cse15l-w22.github.io/");
+        expected.add("https://cse.ucsd.edu/");
+        List<String> list = List.of("snippet3.md");
+
+        for (String string : list) {
+            Path fileName = Path.of(string);
+	        String contents = Files.readString(fileName);
+            ArrayList<String> links = MarkdownParse.getLinks(contents);
+            assertEquals(expected, links);
+        }
+    }
+
+
+
+
+
+
+
+
+
     @Test
     public void tester() throws IOException {
         ArrayList<String> expected = new ArrayList<>();
